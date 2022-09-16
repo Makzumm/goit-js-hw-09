60a026fcdb71add86a5925e0cf0e04b988e7eb7e
Maksym <lucky13375@gmail.com>
2022-09-16 19:28:46 +0200

    finished 1st ht

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const colorOutput = document.querySelector('.hex-output');
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+    intervalSetup();
+})
+
+function intervalSetup() {
+    startBtn.setAttribute('disabled', true),
+    stopBtn.removeAttribute('disabled');
+
+    timerId = setInterval(() => {
+        return document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    stopBtn.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
